eddda13bf06cf999b9e3b9bb27c9fbcaf5bbaff4
Megan Lin <megan.w.lin@gmail.com>
2017-11-06 10:39:24 -0500

    move js

---
+++ PastPhotos/gallery.js
@@ -0,0 +1,8 @@
+         for(var i=1; i<=178; i++){
+            var x=document.createElement("IMG");
+            x.setAttribute("src", "Techstrav2016/"+i+"techstrav.JPG");
+            x.setAttribute("width", "auto");
+            x.setAttribute("height","150");
+            x.setAttribute("id", i+"");
+            document.getElementById("pics").appendChild(x);
+        }
